f7fec314df35302967767e008bf0ca47122207a2
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-13 11:52:05 -0700

    kfj

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,7 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
+import math
+def main(a):
+    punish = math.pi(a,2)
+    return punish   
